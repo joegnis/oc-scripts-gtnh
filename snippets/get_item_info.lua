1c69654f9421b0e6f070cefd19d774defc84d223
Kai Liao <joegnis@pm.me>
2024-01-29 17:40:14 -0800

    Adds --side option to get_item_info; also improves searching
    Now it searches for the first stack

--- snippets/get_item_info.lua
+++ snippets/get_item_info.lua
@@ -1,18 +1,37 @@
 local component = require "component"
 local sides = require "sides"
 
+local function firstStack(transposer, sideInventory)
+    local stackIterator = transposer.getAllStacks(sideInventory)
+    local curStack = stackIterator()
+    local curSlot = 1
+    while curStack ~= nil do
+        if next(curStack) ~= nil then
+            return curStack
+        end
 
-local args = { ... }
-if args[1] == "-h" or args[1] == "--help" then
-    print("Prints info about the first item in the chest on top of the (primary) transposer.")
-    os.exit(0)
+        curStack = stackIterator()
+        curSlot = curSlot + 1
+    end
 end
 
 -- Change this if needed
 local transposer = component.transposer
 --local transposer = component.proxy("ID")
--- Change this if needed
 local chestSide = sides.top
+
+local args = { ... }
+if args[1] == "-h" or args[1] == "--help" then
+    print("Prints info about the first item in the chest on top of the (primary) transposer.")
+    os.exit(0)
+elseif args[1] == "--side" then
+    if args[2] then
+        chestSide = sides[args[2]]
+    else
+        print("Specify a side of the transposer.")
+        os.exit(1)
+    end
+end
 
 local function tableToString(obj)
     local function toString(o)
@@ -33,5 +52,5 @@ local function tableToString(obj)
     return toString(obj)
 end
 
-local stackInfo = transposer.getStackInSlot(chestSide, 1)
+local stackInfo = firstStack(transposer, chestSide)
 print(tableToString(stackInfo))
